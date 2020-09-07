f089631ad837cdfc4d34ed210d6161e8e1d30060
jadsonpp <jadson_pereira5@hotmail.com>
2020-09-07 17:52:16 -0300

    Correção pagina de adoção e info pet

--- src/pages/BuscaPet/index.js
+++ src/pages/BuscaPet/index.js
@@ -1,17 +1,82 @@
 import React from 'react';
-import {FiSearch} from 'react-icons/fi/'
-import "./styles.css"
+import "./styles.css";
+import Cabecalho from "../Cabecalho";
+import { Link } from 'react-router-dom';
+import  viralataCaramel1 from '../../assets/imagens/viralataCaramel1.jpg';
 
 //npm install @material-ui/core --save
 
 
 export default function BuscaPet(){
+  const nome = "Caramelin";
+  const raca = "Raça não identificada";
+  const idade = "6 meses";
+  const cidade = "Vitória"
 
-  
     return (
-      <div classeName="busca-container">
-      <input type="search"></input>  
-      <FiSearch size={30} color="black"> Buscar Pet</FiSearch>
-      </div>
+      <div >
+          <Cabecalho/>
+        <div className="busca-pet-container">
+
+
+            <h1>Animais de {cidade}</h1>
+            <ul>
+                <li>
+                <Link to="/infoPet">  
+                <img src={viralataCaramel1} alt="Logo adotei"/> 
+                
+                <div className="busca-pet-topo-retangulo-container">
+                                <h1>{nome}</h1>
+                                <h2>{raca}</h2>
+                                <h2>{idade}</h2>
+                        </div>
+                        <div className="busca-pet-baixo-retangulo-container"></div>
+                        </Link>
+                </li>
+                
+                <li>
+                <Link to="/infoPet">  
+                <img src={viralataCaramel1} alt="Logo adotei"/> 
+                
+                <div className="busca-pet-topo-retangulo-container">
+                                <h1>{nome}</h1>
+                                <h2>{raca}</h2>
+                                <h2>{idade}</h2>
+                        </div>
+                        <div className="busca-pet-baixo-retangulo-container"></div>
+                        </Link>
+                </li>
+
+                <li>
+                <Link to="/infoPet">  
+                <img src={viralataCaramel1} alt="Logo adotei"/> 
+                
+                <div className="busca-pet-topo-retangulo-container">
+                                <h1>{nome}</h1>
+                                <h2>{raca}</h2>
+                                <h2>{idade}</h2>
+                        </div>
+                        <div className="busca-pet-baixo-retangulo-container"></div>
+                        </Link>
+                </li>
+                
+                <li>
+                <Link to="/infoPet">  
+                <img src={viralataCaramel1} alt="Logo adotei"/> 
+                
+                <div className="busca-pet-topo-retangulo-container">
+                                <h1>{nome}</h1>
+                                <h2>{raca}</h2>
+                                <h2>{idade}</h2>
+                        </div>
+                        <div className="busca-pet-baixo-retangulo-container"></div>
+                        </Link>
+                </li>
+            </ul>
+            
+        </div>
+        </div>
+        
+
     );
 }
--- src/pages/InfoPet/index.js
+++ src/pages/InfoPet/index.js
@@ -9,21 +9,20 @@ import virlata3 from "../../assets/imagens/virlata3.jpg"
 
 
 export default function InfoPet(){
-
+    const textoInfo = `Lorem ipsum tempus ultricies inceptos vulputate faucibus massa, torquent fusce taciti praesent quisque augue varius aptent, felis scelerisque senectus sapien enim aenean. praesent consectetur condimentum nec nam integer nam cras justo pretium, venenatis malesuada nostra ut volutpat habitant eros aenean nostra, viverra posuere mollis conubia integer et eros felis. fringilla vulputate luctus facilisis sociosqu integer phasellus quam nam euismod tellus velit quisque, primis ut elit eget aliquet lacinia potenti inceptos facilisis ullamcorper. accumsan molestie nisl viverra lectus nostra at pharetra malesuada tincidunt hendrerit gravida, erat himenaeos primis dolor semper rhoncus purus velit class cubilia sem, non rhoncus tristique sem eu cubilia vehicula velit lectus elit.
+    Para adotalo entre em contato com o numero: 123456 `
  return(
     <div>
     <Cabecalho/>
         <div>
-            <AliceCarousel autoPlay autoPlayInterval="3000">
+            <AliceCarousel autoPlay autoPlayInterval="3500">
                 <img src={viralataCaramel1} alt="Logo adotei"/>
                 <img src={viralataamarelo2} alt="Logo adotei"/>
                 <img src={virlata3} alt="Logo adotei"/>
             </AliceCarousel>
             <div className="info-container">
                 
-            <p>Lorem ipsum tempus ultricies inceptos vulputate faucibus massa, torquent fusce taciti praesent quisque augue varius aptent, felis scelerisque senectus sapien enim aenean. praesent consectetur condimentum nec nam integer nam cras justo pretium, venenatis malesuada nostra ut volutpat habitant eros aenean nostra, viverra posuere mollis conubia integer et eros felis. fringilla vulputate luctus facilisis sociosqu integer phasellus quam nam euismod tellus velit quisque, primis ut elit eget aliquet lacinia potenti inceptos facilisis ullamcorper. accumsan molestie nisl viverra lectus nostra at pharetra malesuada tincidunt hendrerit gravida, erat himenaeos primis dolor semper rhoncus purus velit class cubilia sem, non rhoncus tristique sem eu cubilia vehicula velit lectus elit.
-            Para adotalo entre em contato com o numero: 123456 
-            </p>
+                <p>{textoInfo}</p>
             </div>
         </div>
     </div>    
